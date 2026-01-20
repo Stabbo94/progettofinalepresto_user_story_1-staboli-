9f4e055aa9ab64ff46753e82fea5471522b5e654
Alberto Staboli <albertostaboli@gmail.com>
2026-01-20 19:08:07 +0100

    US 8 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
@@ -41,51 +42,53 @@ class CreateArticleForm extends Component
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
                 
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                    ])->dispatch($newImage->id);
+                }
+                
+                File::deleteDirectory(storage_path('/app/livewire-tmp'));
             }
             
-            File::deleteDirectory(storage_path('/app/livewire-tmp'));
+            session()->flash('success', 'Articolo creato correttamente');
+            $this->cleanForm();
         }
         
-        session()->flash('success', 'Articolo creato correttamente');
-        $this->cleanForm();
-    }
-    
-    
-    public function render()
-    {
-        return view('livewire.create-article-form');
-    }
-    
-    public function updatedTemporaryImages()
-    {
-        if ($this->validate([
-            'temporary_images.*' => 'image|max:1024',
-            'temporary_images' => 'max:6'
-            ])) {
-                foreach ($this->temporary_images as $image) {
-                    $this->images[] = $image;
+        
+        public function render()
+        {
+            return view('livewire.create-article-form');
+        }
+        
+        public function updatedTemporaryImages()
+        {
+            if ($this->validate([
+                'temporary_images.*' => 'image|max:1024',
+                'temporary_images' => 'max:6'
+                ])) {
+                    foreach ($this->temporary_images as $image) {
+                        $this->images[] = $image;
+                    }
                 }
             }
-        }
-        
-        public function removeImage($key)
-        {
-            if (in_array($key, array_keys($this->images))) {
-                unset($this->images[$key]);
+            
+            public function removeImage($key)
+            {
+                if (in_array($key, array_keys($this->images))) {
+                    unset($this->images[$key]);
+                }
             }
+            
+            protected function cleanForm()
+            {
+                $this->title = '';
+                $this->description = '';
+                $this->category = '';
+                $this->price = '';
+                $this->images = [];
+            }
+            
         }
         
-        protected function cleanForm()
-        {
-            $this->title = '';
-            $this->description = '';
-            $this->category = '';
-            $this->price = '';
-            $this->images = [];
-        }
-        
-    }
-    
